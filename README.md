76794e81da992aab1b5b497a7ec2719b17d6e432
Zacharias Stankiewicz <zacharias.stankiewicz@gmail.com>
2018-09-19 13:14:04 -0400

    basic building blocks

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Devops Projet
+#devops-project
+
+Given the use case of a static web application, I'm choosing to host the content from S3. I chose S3 because it is free-tier, serverless (which means less security surface area), and fast. 
+
+I chose an ALB to front the s3 bucket to ensure the ssl/https setup wasn't strictly coupled to a target, for example, if the config was strictly 
+
+Given the time budget of one hour I was given, I decided not to spin up a fargate task and associated IAM roles.
+
+## Reproducing in your own AWS environment
+
+These steps assume you have terraform installed.
+
+0. Setup varibales for your VPC id, 
+1. Setup a route53 hosted zone. Put that info into the variables.tf file.
+2. Setup a let's encrypt certificate for that domain
+3. Put the key file in a place where 
+
+## Production Concerns (or, things I would do with days instead of a couple of hours)
+- Write unit tests for the TF code before things even harder to test: https://github.com/newcontext-oss/kitchen-terraform
+- Do ALB logging to S3, could ship to Cloudwatch for alerting via SNS.
+- As soon as there's IP-secured assets, it would be wise to setup a non-standard VPC to secure and scope the network layer.
+
+## Next steps
+- Replace the S3 target with an ECS container for a frontend container like angularcli/nginx
---
+++ environment/alb.tf
@@ -0,0 +1,90 @@
+# In Terraform, ALBs are small because all the action is in listeners, targets, and security groups
+resource "aws_alb" "alb-webapplication" {
+  name            = "webapplication"
+  subnets         = ["${var.alb_public_subnets}"]
+  security_groups = ["${aws_security_group.alb-webapplication-external_sg.id}"]
+
+  access_logs {
+    bucket  = "${aws_s3_bucket.webapp-elb-logs.id}"
+    enabled = true
+  }
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "${var.domain_name}"
+  validation_method = "DNS"
+}
+
+resource "aws_alb_target_group" "webapplication-targetgroup" {
+  name                 = "webapplication"
+  port                 = 80
+  protocol             = "HTTP"
+  vpc_id               = "${var.vpc_id}"
+  deregistration_delay = 3
+  proxy_protocol_v2    = true
+}
+
+resource "aws_alb_listener" "webapplication-alb-listener" {
+  load_balancer_arn = "${aws_alb.alb-webapplication.id}"
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = "${aws_acm_certificate.cert.certificate_arn}"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.webapplication-targetgroup.id}"
+    type             = "forward"
+  }
+
+  #sometimes this needs to be put in. sometimes terraform is better at it.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_listener" "webapplication-redirect-https" {
+  load_balancer_arn = "${aws_alb.alb-webapplication.id}"
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+      path = "/"
+      host = "#{host}"
+    }
+  }
+}
+
+resource "aws_security_group" "alb-webapplication-external_sg" {
+  description = "defines specific varieties of access"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # it's necessary to accept 80 in order to automatically upgrade the http request
+  # if this was internal, i'd go 443 only.
+  ingress {
+    protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+}
